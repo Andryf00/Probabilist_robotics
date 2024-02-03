943aadb9c4e722301ebe0b651a33056e371bafb0
Andryf00 <andreaferrari00@gmail.com>
2024-02-03 12:32:45 +0100

    adjust plotting so that the rectangle center is on the ground truth position instead of the bottom left corner, scatter landmarks

--- plot_trajectory.py
+++ plot_trajectory.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle, Circle
 import matplotlib.animation as animation
 import math
 import numpy as np
@@ -23,27 +23,44 @@ gt_array = np.array(ground_truth_poses)
 odo_array = np.array(odometry_poses)
 
 
-patch = Rectangle((0,0),0.2,0.3,
+patch = Rectangle((0,0),0.3,0.3,
                     angle=math.degrees(0),
                     edgecolor='red',
                     facecolor='none',
                     lw=4)
 
+patch_max_distance = Circle((0,0), radius=5, facecolor='none', edgecolor='red')
+
 fig = plt.figure()
-plt.axis('equal')
-plt.grid()
 ax = fig.add_subplot()
+ax.grid()
+ax.set_aspect('equal')
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+
 ax.add_patch(patch)
+ax.add_patch(patch_max_distance)
+
+
 
 ax.plot(gt_array[:,0], gt_array[:, 1])
 ax.plot(odo_array[:,0], odo_array[:, 1])
-#ax.scatter(landmark_array[:, 0], landmark_array[:, 1])
+ax.scatter(landmark_array[:, 0], landmark_array[:, 1],
+           facecolors = 'none', edgecolors='g')
 
 def animate(i):
-    patch.set_xy((gt_array[i,0], gt_array[i,1]))
-    patch.set_angle(math.degrees(gt_array[i,2]))
+
+    angle_rad = gt_array[i,2]
+    dx = 0.15*np.cos(angle_rad) - 0.15*np.sin(angle_rad)
+    dy = 0.15*np.sin(angle_rad) + 0.15*np.cos(angle_rad)
+    x = gt_array[i,0] - dx
+    y = gt_array[i,1] - dy
+    patch.set_xy((x, y))
+    patch.set_angle(math.degrees(angle_rad))
+    patch_max_distance.set_center((gt_array[i,0], gt_array[i,1]))
+    
     #print(patch._angle)
-    return patch,
+    return patch, patch_max_distance
 
 anim = animation.FuncAnimation(fig, animate,
                                frames=len(ground_truth_poses),
